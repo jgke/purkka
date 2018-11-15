9e3a5ce0b850e7f399228a390aa59ea2629258bf
Jaakko Hannikainen <jgke@jgke.fi>
2018-11-15 22:44:07 +0200

    Output enum names

--- src/lalr/src/lib.rs
+++ src/lalr/src/lib.rs
@@ -1,22 +1,22 @@
-#![crate_type="dylib"]
+#![crate_type = "dylib"]
 #![feature(plugin_registrar, rustc_private)]
 
-extern crate syntax;
-extern crate syntax_pos;
 extern crate rustc;
 extern crate rustc_plugin;
 extern crate smallvec;
+extern crate syntax;
+extern crate syntax_pos;
 
-use syntax::parse::token;
-use syntax::ast;
-use syntax::tokenstream::TokenTree;
-use syntax::ext::base::{ExtCtxt, MacResult, DummyResult, MacEager};
-use syntax::ext::build::AstBuilder;
-use syntax::ext::quote::rt::Span;
-use syntax_pos::DUMMY_SP;
 use rustc_plugin::Registry;
 use smallvec::SmallVec;
+use syntax::ast;
+use syntax::ext::base::{DummyResult, ExtCtxt, MacEager, MacResult};
+use syntax::ext::build::AstBuilder;
+use syntax::ext::quote::rt::Span;
+use syntax::parse::token;
 use syntax::ptr::P;
+use syntax::tokenstream::TokenTree;
+use syntax_pos::DUMMY_SP;
 
 fn is_semi_r(tree: Option<&TokenTree>) -> bool {
     match tree {
@@ -33,7 +33,7 @@ fn is_semi(tree: Option<&&TokenTree>) -> bool {
 }
 
 fn expand_rn(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
-        -> Box<MacResult + 'static> {
+             -> Box<MacResult + 'static> {
     let mut iter = args.iter().peekable();
 
     let mut items = SmallVec::<[P<ast::Item>; 1]>::new();
@@ -54,10 +54,14 @@ fn expand_rn(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
                 return DummyResult::any(sp);
             }
         }
+
+        let mut variants = Vec::new();
+
         while !is_semi(iter.peek()) {
             match iter.next() {
-                Some(TokenTree::Token(s, token::Ident(_, _))) => {
+                Some(TokenTree::Token(s, token::Ident(t, _))) => {
                     rsp = *s;
+                    variants.push(cx.variant(*s, *t, Vec::new()));
                 }
                 Some(TokenTree::Token(span, token)) => {
                     let s: Span = *span;
@@ -75,7 +79,7 @@ fn expand_rn(cx: &mut ExtCtxt, sp: Span, args: &[TokenTree])
 
         let (s, t) = ident;
 
-        items.push(cx.item_enum(s.to(rsp), *t, ast::EnumDef {variants: Vec::new()}));
+        items.push(cx.item_enum(s.to(rsp), *t, ast::EnumDef { variants }));
     }
 
     MacEager::items(items)
