6e538f5233d3f866af370d6745924c7fdb824b43
Jaakko Hannikainen <jgke@jgke.fi>
2019-01-02 15:16:28 +0200

    Progress

--- src/preprocessor/tests/macros.rs
+++ src/preprocessor/tests/macros.rs
@@ -1,7 +1,8 @@
 extern crate preprocessor;
 extern crate shared;
 
-use preprocessor::{MacroToken, MacroTokenType};
+use preprocessor::tokentype::Operator;
+use preprocessor::tokenizer::{MacroToken, MacroTokenType};
 use shared::fragment::{Source, Span};
 
 fn process(original: &str, expected: Vec<MacroToken>) {
@@ -53,4 +54,47 @@ fn strings() {
             MacroTokenType::StringLiteral("foo".to_string())),
         mt("foo.c", 6, 15, 
             MacroTokenType::StringLiteral("bar\nbaz".to_string()))]);
+}
+
+#[test]
+fn numbers() {
+    process(
+        "1 .54e+1..a5",
+        vec![
+        mt("foo.c", 0, 0, 
+            MacroTokenType::Number("1".to_string())),
+        mt("foo.c", 2, 11, 
+            MacroTokenType::Number(".54e+1..a5".to_string()))]);
+}
+
+#[test]
+fn period() {
+    process(
+        ". .a",
+        vec![
+        mt("foo.c", 0, 0, 
+            MacroTokenType::Operator(Operator::Dot)),
+        mt("foo.c", 2, 2, 
+            MacroTokenType::Operator(Operator::Dot)),
+        mt("foo.c", 3, 3, 
+            MacroTokenType::Identifier("a".to_string()))
+        ]);
+}
+
+#[test]
+fn comments() {
+    process(
+        "/* foo */ bar //baz \nqux // bax \\\nbux",
+        vec![
+        mt("foo.c", 10, 12, 
+            MacroTokenType::Identifier("bar".to_string())),
+        mt("foo.c", 21, 23, 
+            MacroTokenType::Identifier("qux".to_string()))]);
+}
+
+#[test]
+fn whitespace() {
+    process(
+        "\t   \t \\\n \n   ",
+        vec![]);
 }
