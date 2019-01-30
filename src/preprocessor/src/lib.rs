7c2bd2a7d4d3fc9acc154ecf5a0730f854829227
Jaakko Hannikainen <jgke@jgke.fi>
2019-01-30 14:52:46 +0200

    Global paths

--- src/preprocessor/src/lib.rs
+++ src/preprocessor/src/lib.rs
@@ -6,8 +6,11 @@ pub mod tokentype;
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::path;
 
 use tokenizer::{MacroContext, MacroToken, ParseResult};
+
+static INCLUDE_PATH: &[&str] = &["/usr/local/include", "/usr/include"];
 
 pub fn preprocess<CB>(get_file: CB, filename: &str) -> ParseResult<Vec<MacroToken>>
 where
@@ -17,12 +20,28 @@ where
 }
 
 pub fn preprocess_file(filename: &str) -> ParseResult<Vec<MacroToken>> {
-    let get_file = |is_local, current_file, filename| {
+    let get_file = |is_local, current_file, filename: String| {
         let mut contents = String::new();
-        let mut f = File::open(filename).expect("file not found");
-        f.read_to_string(&mut contents)
-            .expect("something went wrong reading the file");
-        contents
+        if is_local {
+            let mut path = path::PathBuf::from(current_file);
+            path.pop();
+            path.push(filename.clone());
+            if let Ok(mut f) = File::open(path) {
+                f.read_to_string(&mut contents)
+                    .expect("something went wrong reading the file");
+                return contents
+            }
+        }
+        for std_path in INCLUDE_PATH {
+            let mut path = path::PathBuf::from(std_path);
+            path.push(filename.clone());
+            if let Ok(mut f) = File::open(path) {
+                f.read_to_string(&mut contents)
+                    .expect("something went wrong reading the file");
+                return contents
+            }
+        }
+        panic!("File {} not found", filename);
     };
 
     preprocess(get_file, filename)
