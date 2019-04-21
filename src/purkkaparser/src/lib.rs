600dfcbcbda81f00ed6e0fefb223205710bc4e2b
Jaakko Hannikainen <jgke@jgke.fi>
2019-04-21 21:06:12 +0300

    Add a AST visitor for Purkka

--- src/purkkaparser/src/lib.rs
+++ src/purkkaparser/src/lib.rs
@@ -8,6 +8,7 @@
 pub mod parser;
 pub mod token;
 pub mod tokenizer;
+pub mod visitor;
 
 use parser::{parse, S};
 use tokenizer::tokenize;
---
+++ src/purkkaparser/src/visitor.rs
@@ -0,0 +1,171 @@
+use std::ops::DerefMut;
+
+use crate::parser::*;
+
+pub trait ASTVisitor {
+    fn visit_s(&mut self, s: &mut S) { walk_s(self, s); }
+    fn visit_translation_unit(&mut self, s: &mut TranslationUnit) { walk_translation_unit(self, s); }
+    fn visit_unit(&mut self, s: &mut Unit) { walk_unit(self, s); }
+    fn visit_declaration(&mut self, s: &mut Declaration) { walk_declaration(self, s); }
+    fn visit_ty(&mut self, s: &mut TypeSignature) {}
+    fn visit_assignment(&mut self, s: &mut Assignment) { walk_assignment(self, s); }
+    fn visit_expression(&mut self, s: &mut Expression) { walk_expression(self, s); }
+    fn visit_primary_expression(&mut self, s: &mut PrimaryExpression) { walk_primary_expression(self, s); }
+    fn visit_literal(&mut self, s: &mut Literal) {}
+    fn visit_block_expression(&mut self, s: &mut BlockExpression) { walk_block_expression(self, s); }
+    fn visit_block(&mut self, s: &mut Block) { walk_block(self, s); }
+    fn visit_conditional_expression(&mut self, s: &mut ConditionalExpression) {
+        walk_conditional_expression(self, s);
+    }
+    fn visit_statement(&mut self, s: &mut Statement) { walk_statement(self, s); }
+    fn visit_lambda(&mut self, s: &mut Lambda) { walk_lambda(self, s); }
+    fn visit_param(&mut self, s: &mut Param) { walk_param(self, s); }
+}
+
+pub fn walk_s<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut S) {
+    match s {
+        S::TranslationUnit(tu) => visitor.visit_translation_unit(tu)
+    }
+}
+
+pub fn walk_translation_unit<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut TranslationUnit) {
+    match s {
+        TranslationUnit::Units(units) => units.iter_mut().for_each(|i| visitor.visit_unit(i))
+    }
+}
+
+pub fn walk_unit<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Unit) {
+    match s {
+        Unit::Declaration(decl) => visitor.visit_declaration(decl),
+        Unit::IncludeFile(_) => unimplemented!(),
+        Unit::Typedef(_) => unimplemented!(),
+    }
+}
+
+pub fn walk_declaration<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Declaration) {
+    match s {
+        Declaration::Declaration(_, _, _, ty, assignment) => {
+            if let Some(ty) = ty {
+                visitor.visit_ty(ty);
+            }
+            if let Some(assignment) = assignment {
+                visitor.visit_assignment(assignment);
+            }
+        }
+    }
+}
+
+pub fn walk_assignment<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Assignment) {
+    match s {
+        Assignment::Expression(expr) => {
+            visitor.visit_expression(expr);
+        }
+    }
+}
+
+pub fn walk_expression<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Expression) {
+    match s {
+        Expression::PrimaryExpression(expr) => {
+            visitor.visit_primary_expression(expr);
+        }
+        Expression::Op(_op, ExprList::List(exprs)) => {
+            exprs.iter_mut().for_each(|e| visitor.visit_expression(e));
+        }
+        Expression::Unary(_op, ExprList::List(exprs)) => {
+            exprs.iter_mut().for_each(|e| visitor.visit_expression(e));
+        }
+        Expression::Prefix(_op, expr) => {
+            visitor.visit_expression(expr);
+        }
+    }
+}
+
+pub fn walk_primary_expression<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut PrimaryExpression) {
+    match s {
+        PrimaryExpression::Identifier(_ident) => {}
+        PrimaryExpression::Call(_, ArgList::Args(ref mut args)) => {
+            args.iter_mut().for_each(|ref mut a| visitor.visit_expression(a.deref_mut()));
+        }
+        PrimaryExpression::Literal(literal) => {
+            visitor.visit_literal(literal);
+        }
+        PrimaryExpression::BlockExpression(block) => {
+            visitor.visit_block_expression(block.deref_mut());
+        }
+        PrimaryExpression::Expression(expr) => {
+            visitor.visit_expression(expr.deref_mut());
+        }
+        PrimaryExpression::Lambda(lambda) => {
+            visitor.visit_lambda(lambda);
+        }
+    }
+}
+
+pub fn walk_block_expression<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut BlockExpression) {
+    match s {
+        BlockExpression::Block(block) => {
+            visitor.visit_block(block);
+        }
+        BlockExpression::ConditionalExpression(cond) => {
+            visitor.visit_conditional_expression(cond)
+        }
+    }
+}
+
+pub fn walk_conditional_expression<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut ConditionalExpression) {
+    match s {
+        ConditionalExpression::Exprs(arms, otherwise) => {
+            arms.iter_mut().for_each(|(condition, arm)| {
+                visitor.visit_expression(condition.deref_mut());
+                visitor.visit_block(arm.deref_mut());
+            });
+            otherwise.iter_mut().for_each(|b| visitor.visit_block(b.deref_mut()));
+        }
+    }
+}
+
+pub fn walk_block<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Block) {
+    match s {
+        Block::Statements(statements) => {
+            statements.iter_mut().for_each(|statement| visitor.visit_statement(statement));
+        }
+    }
+}
+
+pub fn walk_statement<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Statement) {
+    match s {
+        Statement::Declaration(decl) => {
+            visitor.visit_declaration(decl);
+        }
+        Statement::BlockExpression(block_expression) => {
+            visitor.visit_block_expression(block_expression);
+        }
+        Statement::Expression(expression) => {
+            visitor.visit_expression(expression);
+        }
+        Statement::Return(maybe_expression) => {
+            maybe_expression.iter_mut().for_each(|b| visitor.visit_expression(b));
+        }
+    }
+}
+
+pub fn walk_lambda<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Lambda) {
+    match s {
+        Lambda::Lambda(params, ty, block_expr) => {
+            params.iter_mut().for_each(|p| visitor.visit_param(p));
+            visitor.visit_ty(ty);
+            visitor.visit_block_expression(block_expr);
+        }
+    }
+}
+
+pub fn walk_param<T: ASTVisitor + ?Sized>(visitor: &mut T, s: &mut Param) {
+    match s {
+        Param::Param(_, ty) => {
+            visitor.visit_ty(ty);
+        }
+        Param::Anon(ty) => {
+            visitor.visit_ty(ty);
+        }
+    }
+}
