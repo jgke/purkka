1815839f4057c114156beb69dcd94828744148d7
Jaakko Hannikainen <jgke@jgke.fi>
2019-07-11 18:43:41 +0300

    Remove C error from expressions

--- src/core/tests/testcases/expressions.c
+++ src/core/tests/testcases/expressions.c
@@ -4,8 +4,8 @@ int main() {
     a = 0 ? 1 : 2;
     a = 1 ? 1 : 2;
     int *b = &a;
-    int *c = b + 1;
-    int *d = b - 1;
+    int *c = b + 1 - 1;
+    int *d = b - 1 + 1;
     int e = *b;
     return a + *b + *c + *d + e;
 }
